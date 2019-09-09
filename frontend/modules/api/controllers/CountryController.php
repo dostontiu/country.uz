e28bf6f7bbbceb7b028d272e390167da64734c2d
Doston <dostonberuniy@gmail.com>
2019-09-09 11:59:58 +0500

    use ActiveController

--- frontend/modules/api/controllers/CountryController.php
+++ frontend/modules/api/controllers/CountryController.php
@@ -3,24 +3,10 @@
 namespace app\modules\api\controllers;
 
 use app\modules\api\models\Country;
+use yii\rest\ActiveController;
 use yii\web\Response;
 
-class CountryController extends \yii\web\Controller
+class CountryController extends ActiveController
 {
-    public $enableCsrfValidation = false;
-
-    public function actionCreate()
-    {
-        \Yii::$app->response->format = Response::FORMAT_JSON;
-        $country = new Country();
-        $country->scenario = Country::SCENARIO_CREATE;
-        $country->attributes = \Yii::$app->request->post();
-
-        if ($country->validate() && $country->save()){
-            return array('status'=>true);
-        } else {
-            return array('status'=>false, 'data' => $country->getErrors());
-        }
-    }
-
+    public $modelClass = "app\modules\api\models\Country";
 }
